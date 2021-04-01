9738469f68c16a3e4c0baf5586fcbff3b0a889f2
lukasdrahy <lukas@drahy.net>
2021-04-01 14:13:59 +0200

    Fix typo

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,7 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
- * @ORM\Entity(repositoryClass=UrlRepository::class)
+ * @ORM\Entity(repositoryClass=UserRepository::class)
  * @ORM\Table(
  *     name="user",
  *     indexes={
