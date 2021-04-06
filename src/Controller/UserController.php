814cd5e55bd88fe99605cb8bcf163a51c89d8db0
lukasdrahy <lukas@drahy.net>
2021-04-06 10:32:10 +0200

    Add doc for User API endpoint

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Repository\UserRepository;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use Nelmio\ApiDocBundle\Annotation\Model;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Swagger\Annotations as SWG;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Exception\BadRequestException;
 use Symfony\Component\Validator\ConstraintViolationListInterface;
@@ -15,6 +17,16 @@ class UserController extends AbstractController
     /**
      * @Rest\Get("/api/user")
      * @Rest\View()
+     *
+     * @SWG\Response(
+     *     response=200,
+     *     description="Returns all Users available",
+     *     @SWG\Schema(
+     *         type="array",
+     *         @Model(type=User::class)
+     *     )
+     * )
+     * @SWG\Tag(name="User")
      */
     public function getAllAction(UserRepository $userRepository): array
     {
@@ -25,6 +37,13 @@ class UserController extends AbstractController
      * @Rest\Get("/api/user/{id}")
      * @Rest\View()
      * @ParamConverter("id", class="App:User")
+     *
+     * @SWG\Response(
+     *     response=200,
+     *     description="Returns user object if available",
+     *     @Model(type=User::class)
+     * )
+     * @SWG\Tag(name="User")
      */
     public function getAction(User $user): User
     {
@@ -35,6 +54,22 @@ class UserController extends AbstractController
      * @Rest\Post("/api/user")
      * @Rest\View()
      * @ParamConverter("user", converter="fos_rest.request_body")
+     *
+     * @SWG\Parameter(
+     *     name="user",
+     *     in="body",
+     *     required=true,
+     *     @SWG\Schema(
+     *         type="array",
+     *         @Model(type=User::class)
+     *     )
+     * )
+     * @SWG\Response(
+     *     response=200,
+     *     description="Returns User object if created",
+     *     @Model(type=User::class)
+     * )
+     * @SWG\Tag(name="User")
      */
     public function postAction(User $user, ConstraintViolationListInterface $validationErrors, UserRepository $userRepository)
     {
@@ -49,6 +84,12 @@ class UserController extends AbstractController
      * @Rest\Delete("/api/user/{id}")
      * @Rest\View()
      * @ParamConverter("id", class="App:User")
+     *
+     * @SWG\Response(
+     *     response=200,
+     *     description="Returns empty array"
+     * )
+     * @SWG\Tag(name="User")
      */
     public function deleteAction(User $user, UserRepository $userRepository): array
     {
