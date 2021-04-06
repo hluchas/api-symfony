8d58ec383e0959e92642e344543738ee8a2be340
lukasdrahy <lukas@drahy.net>
2021-04-06 09:27:03 +0200

    Fix env, docker config, build process, added needed packages

--- build.sh
+++ build.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-set -e
+
 ./dcp build
 cp .env.local.docker .env.local
+
 ./dcp composer install
+
+# Create schemas
 ./dcp php bin/console doctrine:schema:create --env=dev
+./dcp php bin/console doctrine:schema:create --env=test
+
+# Load sample data
+./dcp php bin/console doctrine:fixtures:load --no-interaction
+
+
+
--- test.sh
+++ test.sh
@@ -2,4 +2,9 @@
 set -e
 ./dcp vendor/bin/php-cs-fixer fix src
 ./dcp vendor/bin/phpstan analyse
+
+# This should be done in application logic
+./dcp bin/console doctrine:schema:drop --force --env=test
+./dcp bin/console doctrine:schema:create --env=test
+
 ./dcp vendor/bin/simple-phpunit
