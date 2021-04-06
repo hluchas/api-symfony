06cc525255b816e2fd8bfc05eb2355c078eb8239
lukasdrahy <lukas@drahy.net>
2021-04-06 10:34:39 +0200

    Fix controller action rerurn type

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,11 +5,12 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Repository\UserRepository;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\View\View;
 use Nelmio\ApiDocBundle\Annotation\Model;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Swagger\Annotations as SWG;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Exception\BadRequestException;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Validator\ConstraintViolationListInterface;
 
 class UserController extends AbstractController
@@ -71,13 +72,16 @@ class UserController extends AbstractController
      * )
      * @SWG\Tag(name="User")
      */
-    public function postAction(User $user, ConstraintViolationListInterface $validationErrors, UserRepository $userRepository)
-    {
+    public function postAction(
+        User $user,
+        ConstraintViolationListInterface $validationErrors,
+        UserRepository $userRepository
+    ): View {
         if (count($validationErrors) > 0) {
-            throw new BadRequestException($validationErrors);
+            return View::create($validationErrors, Response::HTTP_BAD_REQUEST);
         }
 
-        return $userRepository->save($user);
+        return View::create($userRepository->save($user), Response::HTTP_OK);
     }
 
     /**
