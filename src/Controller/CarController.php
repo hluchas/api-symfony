72886fadeb1951e009694b770225e9dcb6270a10
lukasdrahy <lukas@drahy.net>
2021-04-06 10:23:07 +0200

    Add doc for Car API endpoint

--- src/Controller/CarController.php
+++ src/Controller/CarController.php
@@ -5,9 +5,12 @@ namespace App\Controller;
 use App\Entity\Car;
 use App\Repository\CarRepository;
 use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\View\View;
+use Nelmio\ApiDocBundle\Annotation\Model;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Swagger\Annotations as SWG;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Exception\BadRequestException;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Validator\ConstraintViolationListInterface;
 
 class CarController extends AbstractController
@@ -15,6 +18,16 @@ class CarController extends AbstractController
     /**
      * @Rest\Get("/api/car")
      * @Rest\View()
+     *
+     * @SWG\Response(
+     *     response=200,
+     *     description="Returns all Cars available",
+     *     @SWG\Schema(
+     *         type="array",
+     *         @Model(type=Car::class)
+     *     )
+     * )
+     * @SWG\Tag(name="Car")
      *
      * @return Car[]
      */
@@ -27,6 +40,13 @@ class CarController extends AbstractController
      * @Rest\Get("/api/car/{id}")
      * @Rest\View()
      * @ParamConverter("id", class="App:Car")
+     *
+     * @SWG\Response(
+     *     response=200,
+     *     description="Returns Url object if available",
+     *     @Model(type=Car::class)
+     * )
+     * @SWG\Tag(name="Car")
      */
     public function getAction(Car $car): Car
     {
@@ -37,13 +57,34 @@ class CarController extends AbstractController
      * @Rest\Post("/api/car")
      * @Rest\View()
      * @ParamConverter("car", converter="fos_rest.request_body")
+     *
+     * @SWG\Parameter(
+     *     name="car",
+     *     in="body",
+     *     required=true,
+     *     @SWG\Schema(
+     *         type="array",
+     *         @Model(type=Car::class)
+     *     )
+     * )
+     * @SWG\Response(
+     *     response=200,
+     *     description="Returns Car object if created",
+     *     @Model(type=Car::class)
+     * )
+     * @SWG\Tag(name="Car")
      */
-    public function postAction(Car $car, ConstraintViolationListInterface $validationErrors, CarRepository $carRepository): Car
-    {
+    public function postAction(
+        Car $car,
+        ConstraintViolationListInterface $validationErrors,
+        CarRepository $carRepository
+    ): View {
         if (count($validationErrors) > 0) {
-            throw new BadRequestException($validationErrors);
+            return View::create($validationErrors, Response::HTTP_BAD_REQUEST);
         }
 
-        return $carRepository->save($car);
+        $carRepository->save($car);
+
+        return View::create($car, Response::HTTP_OK);
     }
 }
