1136e43729cdd093db10e1a3bc8738ff444f39bf
lukasdrahy <lukas@drahy.net>
2021-04-06 09:52:42 +0200

    Add emnpty fixture

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+//        $car = new Car();
+//        $car->setStatus('somestatus');
+//        $car->setFrom(new \DateTime());
+//        $car->setTo(new \DateTime());
+//        $manager->persist($car);
+//
+//        $manager->flush();
+    }
+}
